950208b4785b57ed8503dd37d14da498b052a383
Atbrox <opensource@atbrox.com>
2012-08-23 16:06:29 +0200

    in progress mr code

--- atbrtst/builder/build_atbrtst_mr.py
+++ atbrtst/builder/build_atbrtst_mr.py
@@ -1,4 +1,7 @@
+from operator import itemgetter
 from mrjob.job import MRJob
+
+from operator import itemgetter
 
 from atbr import atbr
 import os
@@ -11,10 +14,11 @@ import logging
 import os.path
 import base64
 from mrjob.protocol import RawValueProtocol
+import time
 
 from patricia_tree import patricia
 
-GH_RESOLUTION=5 # +- 2km
+GH_RESOLUTION = 5 # +- 2km
 
 class MRBuildAtbrTst(MRJob):
     OUTPUT_PROTOCOL = RawValueProtocol
@@ -34,6 +38,15 @@ class MRBuildAtbrTst(MRJob):
         except Exception, e:
             self.increment_counter("mapper", str(e), 1)
 
+    def sort_words(self,words):
+        """sort first due to length and then due to alphanumerical
+       in order to make sure that a prefix come before words that uses
+       the prefix"""
+        len_word_tuples = [(len(w), w) for w in words]
+        #sorted_words = sorted(len_word_tuples, key=itemgetter(0,1))
+        sorted_words = sorted(len_word_tuples, key=itemgetter(1,0))
+        only_sorted_words = [w[1] for w in sorted_words]
+        return only_sorted_words
 
     def reducer(self, shard_key, key_value_pairs):
         try:
@@ -45,24 +58,174 @@ class MRBuildAtbrTst(MRJob):
                     self.keyvalue.put(key, value)
                     self.all_keys.append(key)
         except Exception, e:
-            self.increment_counter("reducer", str(e)+debugval, 1)
-        # TODO: need counters here
+            self.increment_counter("reducer", str(e) + debugval, 1)
+            # TODO: need counters here
 
     def reducer_init(self):
         try:
             self.all_keys = []
             self.patricia_tree = patricia()
             self.keyvalue = atbr.Atbr()
+
         except Exception, e:
-            self.increment_counter("reducer_init", str(e), 1)
+                self.increment_counter("reducer_init", str(e), 1)
+
+    def step_1(self):
+        self.increment_counter("reducer_final", "step1 starting", 1)
+        pos = 0
+        mapped_pos = {}
+        self.all_words = []
+        self.ordering_info = {}
+        t2 = time.time()
+        format = "%%0%dd" % (11)
+        for key in sorted(self.aggregate):
+            slim = ["", ""]
+            if self.mapping.has_key(key):
+                self.aggregate[key]["c"] = self.mapping[key]
+                slim[1] = self.mapping[key]
+            if self.aggregate[key].has_key("value"):
+                slim[0] = self.aggregate[key]["value"]
+
+            self.ordering_info[key] = (slim, self.aggregate[key]["full_val"])
+            self.all_words.append(self.aggregate[key]["full_val"])
+            mapped_pos[key] = format % (pos)
+            data = "%s\n" % (json.dumps(slim))
+            data_len = len(data)
+            record_len = "%011d" % (data_len + 11) # itself!
+            pos += len(record_len) + data_len
+            self.slimmed[key] = (record_len, slim)
+        delta2 = time.time() - t2
+        return mapped_pos
+
+    def step_2(self):
+        self.increment_counter("reducer_final", "step2 starting", 1)
+
+        t3 = time.time()
+        sorted_words = self.sort_words(self.all_words)
+        delta3 = time.time() - t3
+        word_to_order = {}
+        for i, word in enumerate(sorted_words):
+            word_to_order[word] = i
+        #t4 = time.time()
+        print >> sys.stderr, "3. finished", delta3
+        return word_to_order
+
+    def step_3(self, mapped_pos, word_to_order):
+        self.increment_counter("reducer_final", "step3 starting", 1)
+
+        new_records = []
+        slim_sorted = sorted(self.slimmed)
+        address = 0
+        slim = ""
+        for i, key in enumerate(slim_sorted):
+            slim = self.slimmed[key][1]
+            for k in slim[1]:
+                self.slimmed[key][1][1][k] = mapped_pos[slim[1][k]]
+            slim = self.slimmed[key][1]
+            jslim = json.dumps(slim)
+            value = "%s%s\n" % (self.slimmed[key][0], jslim)
+            val_len = len(value)
+            order_info = self.ordering_info[key]
+            order = word_to_order[order_info[1]]
+            new_records.append((order, i, address, order_info[1], val_len,
+                                self.slimmed[key][0], jslim, slim))
+            address += val_len # need to add '\n'
+        #delta4 = time.time() - t4
+        #print >> sys.stderr, "4. finished", delta4
+        return new_records
+
+    def step_4(self, new_records):
+        self.increment_counter("reducer_final", "step4 starting", 1)
+
+        ordered_new_records = None
+        new_map = None
+
+        try:
+            # REORDERING RECORDS ACCORDING TO SORTED ENTRIES
+            t5 = time.time()
+            ordered_new_records = sorted(new_records, key=itemgetter(0))
+            delta5 = time.time() - t5
+            t6 = time.time()
+            address = 0
+            new_map = {}
+            self.increment_counter("reducer_final", "step 4 HERE",1)
+            for record in ordered_new_records:
+                new_record = (address, record)
+                self.increment_counter("reducer_final", "step 4 HERE2",1)
+                value = "%s%s\n" % (record[5], record[6])
+                self.increment_counter("reducer_final", "step 4 HERE3:" + value,1)
+                assert len(value) == record[4]
+                old_address = record[2]
+                new_map[old_address] = (address, record[3])
+                address += record[4]
+            delta6 = time.time() - t6
+            print >> sys.stderr, "5. finished", delta6
+            #return ordered_new_records, new_map
+
+
+        except Exception, e:
+            self.increment_counter("reducer_final", "step4 FAILED:" + str(e), 1)
+            yield "step4failed", new_records
+
+
+    def step_5(self, new_map, ordered_new_records):
+        self.increment_counter("reducer_final", "step5 starting", 1)
+
+        t7 = time.time()
+        # UPDATE TO NEW ADDRESS REFERENCES
+        for record in ordered_new_records:
+            slim = record[7]
+            if type(slim[1]) == dict:
+                for key in slim[1]:
+                    new_address_info = new_map[int(slim[1][key])]
+                    (new_address, _) = new_address_info
+                    format = "%%0%dd" % (11) # HARDCODED!
+                    formatted_new_address = format % (new_address)
+                    slim[1][key] = formatted_new_address
+            value = "%s%s" % (record[5], json.dumps(slim))
+            yield "FOOBAR", value
+            #print value
+        delta7 = time.time() - t7
+        #print >> sys.stderr, "deltas = ", [delta0, delta1, delta2, delta3, delta4, delta5, delta6, delta7]
 
     def reducer_final(self):
         try:
             for key in self.all_keys:
                 self.patricia_tree.addVal(key, key[::-1])
-            yield "finaltree", self.patricia_tree._data
+
+            self.global_block = 1
+            self.num_digits_to_represent_blocks = 11 # int(math.log10(max_num_blocks)+1)
+            self.zeroblock = "".join([str(0) for x in range(self.num_digits_to_represent_blocks)])
+            #yield "finaltree", self.patricia_tree._data
+
+            self.mapping = {}
+            self.aggregate = {}
+            self.partition_tree(self.patricia_tree._data,
+                parent=self.zeroblock,
+                mapping=self.mapping,
+                aggregate=self.aggregate)
+            self.ZEROVAL = u""
+
+            self.aggregate[self.zeroblock] = {'id':self.zeroblock,
+                                              "c":self.mapping[self.zeroblock],
+                                              "full_val": self.ZEROVAL}
+
+            self.slimmed = {}
+
+            mapped_pos = self.step_1()
+            word_to_order = self.step_2()
+            new_records = self.step_3(mapped_pos, word_to_order)
+            ordered_new_records, new_map = self.step_4(new_records)
+
+            self.increment_counter("reducer_final", "after step4 ", 1)
+
+
+
+            #print >> sys.stderr, "2. finished", delta2
+
+            self.step_5(new_map, ordered_new_records)
         except Exception, e:
-            self.increment_counter("reducer_final", str(e), 1)
+                self.increment_counter("reducer_final", str(e), 1)
 
 
     def steps(self):
@@ -71,7 +234,60 @@ class MRBuildAtbrTst(MRJob):
             mapper_final=self.mapper_final,
             reducer=self.reducer,
             reducer_init=self.reducer_init,
-            reducer_final=self.reducer_final),]
+            reducer_final=self.reducer_final), ]
+
+    def partition_tree(self, input, parent,level=0,
+                  mapping={},
+                  aggregate={},
+                  parent_val=u""):
+        """recursive method that unwraps a patricia tree datastructure in
+        a way suitable to represent in for low latency disk lookup
+            (typically ssd)
+            """
+
+        #global global_block
+
+        assert type(input) == dict
+        for key in input:
+            # each val should be an array
+            val = input[key]
+            #print type(val), type(key)
+            assert type(val) == list
+            assert len(val) == 2 or len(val) == 3
+            #print type(val[0]), type(val[1])
+            #print val
+            #print val[0]
+            #print len(val), val[1]
+            assert type(val[0]) == str or type(val[0]) == unicode
+            assert type(val[1]) == dict
+            #print len(val)
+            # 2 cases, len 2 or 3
+            #print len(val)
+            value = None
+            if(len(val)) == 3:
+                (infix, nested, value) = val
+            else:
+                (infix, nested) = val
+
+            full_val = u"%s%s%s" % (parent_val, key, infix)
+
+            data = {"key": key + infix, "p_id": parent, "full_val": full_val}
+            if value != None:
+                data["value"] = value
+
+            if not (key == "" and infix == ""):
+                format = "%%0%dd" % (self.num_digits_to_represent_blocks)
+                data["id"] = format % (self.global_block)
+                if not mapping.has_key(data["p_id"]):
+                    mapping[data["p_id"]] = {}
+                mapping[data["p_id"]][data["key"]] = data["id"]
+                if not aggregate.has_key(data["id"]):
+                    aggregate[data["id"]] = data
+                self.global_block += 1
+
+            format = "%%0%dd" % (self.num_digits_to_represent_blocks)
+            self.partition_tree(nested, level=level + 1, parent=data.get("id", self.zeroblock),
+                mapping=mapping, aggregate=aggregate, parent_val=full_val)
 
 
 if __name__ == '__main__':
